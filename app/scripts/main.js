7bb0744b061aaf8a2ef00a9116167f591ce77062
isthar13 <sylrupenopa@gmail.com>
2016-04-20 22:46:53 +0200

    Bootstrap al repo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,4 +16,30 @@ function loadPage (path) {
 	$('#page').load(path);
 }
 
+// Funcion para validar el formulario de contacto
+function validateForm() {
+	// comprobar que haya valores
+	var nombre = $('#nombre').val();
+	var email = $('#email').val();
+	var telefono = $('#telefono').val();
+	var mensaje = $('#mensaje').val();
+
+	if (nombre === undefined || nombre === '') {
+		$('#grupoNombre').addClass('has-error');
+		$('#nombreError').show();
+	}
+	if (email === undefined || email === '') {
+		$('#grupoEmail').addClass('has-error');
+		$('#emailError').show();
+	}
+	if (mensaje === undefined || mensaje === '') {
+		$('#grupoMensaje').addClass('has-error');
+		$('#mensajeError').show();
+	}
+
+	/* eslint-disable no-alert */
+	alert('Nombre: ' + nombre + '. Correo: ' + email + '. Telefono: ' + telefono + '. Mensaje: ' + mensaje);
+	/* eslint-enable no-alert */
+}
+
 /*eslint-enable no-unused-vars*/
