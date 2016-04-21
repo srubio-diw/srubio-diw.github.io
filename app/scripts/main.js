228f5bd62f512a82fec254f3636dfcd8998240b4
isthar13 <sylrupenopa@gmail.com>
2016-04-21 19:26:08 +0200

    Validación del formulario

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,6 +18,15 @@ function loadPage (path) {
 
 // Funcion para validar el formulario de contacto
 function validateForm() {
+	// Limpio errores previos
+	$('#grupoNombre').removeClass('has-error');
+	$('#nombreError').hide();
+	$('#grupoEmail').removeClass('has-error');
+	$('#emailError').hide();
+	$('#emailInvalidError').hide();
+	$('#grupoEmail').removeClass('has-error');
+	$('#emailError').hide();
+
 	// comprobar que haya valores
 	var nombre = $('#nombre').val();
 	var email = $('#email').val();
@@ -31,15 +40,23 @@ function validateForm() {
 	if (email === undefined || email === '') {
 		$('#grupoEmail').addClass('has-error');
 		$('#emailError').show();
+	} else {
+		// Patrón que pide un correo en formato cuenta@dominio.extension
+		// Siendo cuenta una cadena de texto formada por números, letras, -, _ y .
+		// Siendo dominio una cadena de texto formada por números y letras
+		// Siendo extensión una o dos letras o números
+		var pattern = "/[0-9a-zA-Z_\.-]+@\w+\.\w{2,3}/"
+		if (!(new RegExp(pattern)).test(email)) {
+			$('#grupoEmail').addClass('has-error');
+			$('#emailInvalidError').show();
+		}
 	}
 	if (mensaje === undefined || mensaje === '') {
 		$('#grupoMensaje').addClass('has-error');
 		$('#mensajeError').show();
 	}
 
-	/* eslint-disable no-alert */
-	alert('Nombre: ' + nombre + '. Correo: ' + email + '. Telefono: ' + telefono + '. Mensaje: ' + mensaje);
-	/* eslint-enable no-alert */
+	
 }
 
 /*eslint-enable no-unused-vars*/
