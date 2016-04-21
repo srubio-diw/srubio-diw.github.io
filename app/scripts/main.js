7943ba62d2c0f2a2f100c0653ebeea2b93062df7
isthar13 <sylrupenopa@gmail.com>
2016-04-21 22:09:03 +0200

    Formulario de contacto completo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,13 +19,14 @@ function loadPage (path) {
 // Funcion para validar el formulario de contacto
 function validateForm() {
 	// Limpio errores previos
+	$('#mensajeRecibido').hide();
 	$('#grupoNombre').removeClass('has-error');
 	$('#nombreError').hide();
 	$('#grupoEmail').removeClass('has-error');
 	$('#emailError').hide();
 	$('#emailInvalidError').hide();
-	$('#grupoEmail').removeClass('has-error');
-	$('#emailError').hide();
+	$('#grupoMensaje').removeClass('has-error');
+	$('#mensajeError').hide();
 
 	// comprobar que haya valores
 	var nombre = $('#nombre').val();
@@ -33,30 +34,43 @@ function validateForm() {
 	var telefono = $('#telefono').val();
 	var mensaje = $('#mensaje').val();
 
+	var error = false;
+
 	if (nombre === undefined || nombre === '') {
 		$('#grupoNombre').addClass('has-error');
 		$('#nombreError').show();
+		error = true;
 	}
 	if (email === undefined || email === '') {
 		$('#grupoEmail').addClass('has-error');
 		$('#emailError').show();
+		error = true;
 	} else {
 		// Patrón que pide un correo en formato cuenta@dominio.extension
 		// Siendo cuenta una cadena de texto formada por números, letras, -, _ y .
 		// Siendo dominio una cadena de texto formada por números y letras
-		// Siendo extensión una o dos letras o números
-		var pattern = "/[0-9a-zA-Z_\.-]+@\w+\.\w{2,3}/"
-		if (!(new RegExp(pattern)).test(email)) {
+		// Siendo extensión una o dos letras o números, una o dos veces
+		var pattern = /[0-9a-zA-Z_\.-]+\@\w+(\.\w{2,3}){1,2}/;
+		var regexp = new RegExp(pattern);
+		if (!regexp.test(email)) {
 			$('#grupoEmail').addClass('has-error');
 			$('#emailInvalidError').show();
+			error = true;
 		}
 	}
 	if (mensaje === undefined || mensaje === '') {
 		$('#grupoMensaje').addClass('has-error');
 		$('#mensajeError').show();
+		error = true;
 	}
 
-	
+	if (!error) {
+		$('#mensajeRecibido').show();
+		$('#nombre').val(null);
+		$('#telefono').val(null);
+		$('#email').val(null);
+		$('#mensaje').val(null);
+	}
 }
 
 /*eslint-enable no-unused-vars*/
